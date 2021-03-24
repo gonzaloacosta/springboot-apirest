09ff3e7e8539d545cd56dd81384e2fd2b31640f9
Gonzalo Acosta <gonzalo.acosta@semperti.com>
2021-03-24 11:41:06 -0300

    first commit

---
+++ src/main/java/com/example/demo/controller/MessageController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Message;
+import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class MessageController {
+
+    @GetMapping("/message")
+    Message send() {
+            return new Message("Hello World from Spring Boop App");
+    }
+
+    @PostMapping("/message")
+    Message echo (@RequestBody Message message) {
+        return message;
+    }
+}
+
