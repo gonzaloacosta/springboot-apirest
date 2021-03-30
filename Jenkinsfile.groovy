9a10e7568496c4e885a268845a90f7b49c0238c8
Gonzalo Acosta <gonzalo.acosta@semperti.com>
2021-03-30 19:36:41 -0300

    add new message

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -14,6 +14,7 @@ pipeline {
             steps {
                 echo "STAGE1 - Tasks pre Test and build"
                 sh "ls -ltr"
+                deleteDir()
                 checkout scm
             }
         }
