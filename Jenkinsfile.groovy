571b2cb38c7fea2a067fe6f4dc05c45ac1361356
Gonzalo Acosta <gonzalo.acosta@semperti.com>
2021-03-31 13:35:37 -0300

    add check codepipeline

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,48 +1,45 @@
-// Set your project Prefix using your GUID
 // Set variable globally to be available in all stages
-def prefix      = "semperti"
-def app = "rapientrega"
 
-// Set Maven command to always include Nexus Settings
 //def mvnCmd      = "mvn -s ./nexus_settings.xml"
 def mvnCmd      = "mvn"
 
-// Set Development and Production Project Names
-def devEnv  = "${prefix}-${app}-dev"
-def prodEnv = "${prefix}-${app}-prod"
-
-def prodEnvBlue  = "${prefix}-${app}-blue"
-def prodEnvGreen = "${prefix}-${app}-green"
-
 // Set the tag for the development image: version + build number
 def devTag      = "0.0-0"
+
 // Set the tag for the production image: version
 def prodTag     = "0.0"
-def destApp     = "rapientrega"
-def activeApp   = ""
 
 // Artifact
-def dev = "development"
-def prod = "production"
-def artifactDev = "${app}.zip"
-def artifactProd = "${app}.zip"
-def s3ArtifactDev = "${prefix}-${app}-${dev}-s3-backend-artifact"
-def s3ArtifactProd = "${prefix}-${app}-${prod}-s3-backend-artifact"
+def prefix = "semperti"
+def app = "rapientrega"
+def env = "development"
+def artifact = "${app}.zip"
+def s3Artifact = "${prefix}-${app}-${env}-s3-backend-artifact"
+
+// AWS
 //def awsCredentials = "aws-credentials-jenkins-s3"
 def awsCredentials = "aws-credentials-semperti"
 def awsRegion = "us-east-1"
+def awsProfile = semperti
 
-// ElasticBeanstalk Blue/Green
-def ebDevAppBlue = "rapientrega-eb"
-def ebDevEnvBlue = "development"
-def ebDevEnvGreen = "development-green"
-def ebUrlDevBlue = "development.eba-fkx55m2f.us-east-1.elasticbeanstalk.com"
-def ebUrlDevGreen = "development.eba-fkx55m2f.us-east-1.elasticbeanstalk.com"
-def ebUrlDevBluePath = "message"
-def ebUrlDevGreenPath = "message"
+// CodePipeline
+def codepipelineName = "${prefix}-${app}-${env}-pipeline-backend"
+// ElasticBeanstalk
+def ebApp = "${app}-eb"
+def ebEnv = "${env}"
+def ebUrl = "development.eba-fkx55m2f.us-east-1.elasticbeanstalk.com"
+def ebUrlPath = "message"
 
-//def ebProdApp = ""
-//def ebProdEnv = ""
+//def ebAppBlue = "${app}-eb-blue"
+//def ebEnvBlue = "${env}-blue"
+//def ebUrlBlue = "development-blue.eba-fkx55m2f.us-east-1.elasticbeanstalk.com"
+//def ebUrlBlue = "message"
+
+//def ebAppGreen = "${app}-eb-greenlue"
+//def ebEnvGreen = "${env}-green"
+//def ebUrlGreen = "development-green.eba-fkx55m2f.us-east-1.elasticbeanstalk.com"
+//def ebUrlGreen = "message"
+
 
 pipeline {
 
@@ -102,32 +99,33 @@ pipeline {
         }
         stage('Upload Artifact to S3 Bucket') {
             steps {
-                input "Upload Artifact ${artifactDev} to S3 Bucket ${s3ArtifactDev}?"
-                println artifactDev
-                println s3ArtifactDev
+                input "Upload Artifact ${artifact} to S3 Bucket ${s3Artifact}?"
+                println artifact
+                println s3Artifact
                 withAWS(credentials: "${awsCredentials}", region: "${awsRegion}") {
-                    s3Upload(file:"${artifactDev}", bucket:"${s3ArtifactDev}", path:"${artifactDev}")
+                    s3Upload(file:"${artifact}", bucket:"${s3Artifact}", path:"${artifact}")
                 }
             }
         }
         stage('Check CodePipeline Deploy ElasticBeanstalk') {
             steps {
                 echo "Check CodePipeline Running"
+                //Bug Reported: https://plugins.jenkins.io/pipeline-aws/
                 //withAWS(credentials: "${awsCredentials}", region: "${awsRegion}") {
                 //    ebWaitOnEnvironmentHealth(
-                //        applicationName: "${ebDevAppBlue}", 
-                //        environmentName: "${ebDevEnvBlue}",
+                //        applicationName: "${ebApp}", 
+                //        environmentName: "${ebEnv}",
                 //        health: "Green",
                 //        stabilityThreshold: 60
                 //    )
                 //}
                 script {
-                    def ebDeployStatus = sh(script: """aws --profile semperti codepipeline list-action-executions --pipeline-name semperti-rapientrega-development-pipeline-backend | jq '.actionExecutionDetails[] | select(.status=="Succeeded" and .stageName=="Deploy") | .status'""", returnStdout: true).trim()
+                    def ebDeployStatus = sh(script: """aws --profile ${awsProfile} codepipeline list-action-executions --pipeline-name ${codepipelineName} | jq '.actionExecutionDetails[] | select(.status=="Succeeded" and .stageName=="Deploy") | .status'""", returnStdout: true).trim()
                     println("Deploy Status = ${ebDeployStatus}")
-                    //while (ebDeployStatus != "Succeeded") {
-                    //    sleep 5
-                    //    echo "Waiting for ${ebDevAppBlue} to be ready"
-                    //}
+                    while (ebDeployStatus != "Succeeded") {
+                        sleep 5
+                        echo "Waiting for ${ebDeployStatus} to be ready"
+                    }
                 }
             }
         }
@@ -137,7 +135,7 @@ pipeline {
                 timeout(300) {
                     waitUntil {
                         script {
-                            def r = sh script: "curl -s http://${ebUrlDevBlue}/${ebUrlDevBluePath}", returnStatus: true
+                            def r = sh script: "curl -s http://${ebUrl}/${ebUrlPath}", returnStatus: true
                             return (r == 0);
                         }
                     }
