1e5880443219e22ed8ff8bf741221b42bb8cf940
Gonzalo Acosta <gonzalo.acosta@semperti.com>
2021-03-31 11:42:50 -0300

    add check eb environment

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,76 +1,142 @@
+// Set your project Prefix using your GUID
+// Set variable globally to be available in all stages
+def prefix      = "gire"
+def app = "rapientrega"
+
+// Set Maven command to always include Nexus Settings
+//def mvnCmd      = "mvn -s ./nexus_settings.xml"
+def mvnCmd      = "mvn"
+
+// Set Development and Production Project Names
+def devEnv  = "${prefix}-${app}-dev"
+def prodEnv = "${prefix}-${app}-prod"
+
+def prodEnvBlue  = "${prefix}-${app}-blue"
+def prodEnvGreen = "${prefix}-${app}-green"
+
+// Set the tag for the development image: version + build number
+def devTag      = "0.0-0"
+// Set the tag for the production image: version
+def prodTag     = "0.0"
+def destApp     = "rapientrega"
+def activeApp   = ""
+
+// Artifact
+def dev = "development"
+def prod = "production"
+def artifactDev = "${app}.zip"
+def artifactProd = "${app}.zip"
+def s3ArtifactDev = "${prefix}-${app}-${dev}-s3-backend-artifact"
+def s3ArtifactProd = "${prefix}-${app}-${prod}-s3-backend-artifact"
+def awsCredentials = "aws-credentials-jenkins-s3"
+def awsRegion = "us-east-1"
+
+// ElasticBeanstalk Blue/Green
+def ebDevAppBlue = "rapientrega-eb"
+def ebDevEnvBlue = "development"
+def ebDevEnvGreen = "development-green"
+def ebUrlDevBlue = "development.eba-fkx55m2f.us-east-1.elasticbeanstalk.com"
+def ebUrlDevGreen = "development.eba-fkx55m2f.us-east-1.elasticbeanstalk.com"
+def ebUrlDevBluePath = "message"
+def ebUrlDevGreenPath = "message"
+
+//def ebProdApp = ""
+//def ebProdEnv = ""
+
 pipeline {
 
     agent any
     environment {
-        ANSIBLE_HOST_KEY_CHECKING = 'false'
-    	VERSION = "0.1"
-        //AWS_REGION = "us-east-1"
-        //AWS_CREDENTIAL = "aws-credentials-jenkins-s3"
-        //S3_ARTIFACT = "semperti-rapientrega-development-s3-backend-artifact"
-        //S3_ARTIFACT_OBJECT = "rapientrega.zip"
+        //ANSIBLE_HOST_KEY_CHECKING = 'false'
     }
     stages {
-        stage('Stage 1 - Checkout') {
+        stage('Checkout Source') {
             steps {
-                echo "STAGE1 - Checkout"
-                sh "ls -ltr"
-                //deleteDir()
+                // git credentialsId: 'dc69dd47-d601-4cb0-adbe-548c17e15506', url: "http://<gitRepo>/<username>/<repoName>.git"
                 checkout scm
+                script {
+                    def pom = readMavenPom file: 'pom.xml'
+                    def version = pom.version
+                    devTag  = "${version}-" + currentBuild.number
+                    prodTag = "${version}"
+                }
             }
         }
-        stage('Stage 2 - Unit Test') {
+        stage('Build War File') {
             steps {
-                echo "STAGE2 - Unit Test"
-                sh "pwd ; ls -ltr"
-                sh "mvn clean "
-                sh "mvn test"
+                echo "Building version ${devTag}"
+                sh "${mvnCmd} versions:set -DnewVersion=$env.${devTag}"
+                sh "${mvnCmd} clean package -DskipTests=true"
             }
         }
-        stage('Stage 3 - Release & Package') {
+        stage('Unit Tests') {
             steps {
-                echo "STAGE3 - Release & Upload Nexus"
-                sh "mvn versions:set -DnewVersion=$env.VERSION"
-                sh "mvn clean package -DskipTests"
+                echo "Running Unit Tests"
+                sh "${mvnCmd} test"
+                // It displays the results of tests in the Jenkins Overview
+                //step([$class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/TEST-*.xml'])
             }
         }
-        stage('Stage 4 - Create Artifact') {
+        stage('Code Analysis') {
+            steps {
+                script {
+                echo "Running Code Analysis"
+                //sh "${mvnCmd} sonar:sonar -Dsonar.host.url=http://sonarqube:9000/ -Dsonar.projectName=${JOB_BASE_NAME} -Dsonar.projectVersion=${devTag}"
+                }
+            }
+        }
+        stage('Publish to Nexus') {
+            steps {
+                echo "Publish to Nexus"
+                //sh "${mvnCmd} deploy -DskipTests=true -DaltDeploymentRepository=nexus::default::http://nexus:8081/repository/releases"
+            }
+        }
+        stage('Create Zip Artifact') {
             steps {
                 echo "STAGE 4 - Create Artifact"
+                //Test cert, delete this line after the probe.
                 sh "echo test > serco.crt"
                 zip archive: true, glob: 'Dockerfile, serco.crt, target/*.jar', zipFile: 'rapientrega.zip', overwrite: true
             }
         }
-        stage('Stage 5 - Upload Artifact to S3 Bucket') {
+        stage('Upload Artifact to S3 Bucket') {
             steps {
-                echo "STAGE 5 - Upload Artifact to S3 Bucket"
-                withAWS(credentials: "aws-credentials-jenkins-s3", region: "us-east-1") {
-                    s3Upload(file:"rapientrega.zip", 
-                    bucket:"semperti-rapientrega-development-s3-backend-artifact", 
-                    path:"rapientrega.zip")
+                input "Upload Artifact ${artifactName} to S3 Bucket ${s3Artifact}?"
+                withAWS(credentials: "${awsCredentials}", region: "${awsRegion}") {
+                    s3Upload(file:"${artifactDev}", bucket:"${s3ArtifactDev}", path:"${artifactDev}")
                 }
             }
         }
-        stage('Stage 6 - Check CodePipeline Running') {
+        stage('Check CodePipeline Deploy ElasticBeanstalk') {
             steps {
-                echo "STAGE 6 - Check CodePipeline Running"
-                // Wait to manual approve
+                echo "Check CodePipeline Running"
+                withAWS(credentials: "${awsCredentials}", region: "${awsRegion}") {
+                    ebWaitOnEnvironmentStatus(
+                        applicationName: "${ebDevAppBlue}",
+                        environmentName: "${ebDevEnvBlue}"
+                    )
+                }
+                def ebDevAppBlueSucceeded = sh (script: """aws codepipeline list-action-executions --pipeline-name semperti-rapientrega-development-pipeline-backend | jq '.actionExecutionDetails[] | select(.status=="Succeeded" and .stageName=="Deploy") | .status'""", returnStdout: true).trim()
+                println ebDevAppBlueSucceeded
+                
+                while (ebDevAppBlueSucceeded != "Succeeded") {
+                    sleep 5
+                    echo "Waiting for ${ebDevAppBlue} to be ready"
+                }
+            }
+        }
+        stage('Check Application is Up and Running') {
+            steps {
+                echo "Check CodePipeline Running"
                 timeout(300) {
                     waitUntil {
                         script {
-                            def r = sh script: 'curl -s http://development.eba-fkx55m2f.us-east-1.elasticbeanstalk.com/message', returnStatus: true
+                            def r = sh script: 'curl -s http://${ebUrlDevBlue}/message', returnStatus: true
                             return (r == 0);
                         }
                     }
                 }
             }
         }
-        //stage('Stage 7 - Check Application RUN') {
-            //steps {
-                //echo 'STAGE 7 - Check Application RUN'
-                //steps {
-                //    sh "curl -s http://development.eba-fkx55m2f.us-east-1.elasticbeanstalk.com/message"
-                //}
-            //}
-        //}
     }
 }
